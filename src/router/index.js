4cecc6ba7f353b544dd9bdb0744105fbd67fa625
yaoyan <yaoyan@yaoyanCH-MBA-9H3QF.local>
2017-10-16 13:57:35 +0800

    修复发布bug

--- src/router/index.js
+++ src/router/index.js
@@ -3,13 +3,12 @@ import Router from 'vue-router'
 import List from '@/components/List'
 import Content from '@/components/Content'
 import Login from '@/components/yLogin'
-import Validator from 'vue-validator'
 import add from '@/components/add'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-default/index.css'
 Vue.use(ElementUI)
 Vue.use(Router)
-Vue.use(Validator)
+
 export default new Router({
   mode: 'history',
   routes: [
